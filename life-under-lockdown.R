cd41703a480805e36f715209ea5bf2bd3723510c
CLARISSA BIRINDELLI <98040056+Claribi@users.noreply.github.com>
2022-01-20 23:23:23 +0000

    commit

--- life-under-lockdown.R
+++ life-under-lockdown.R
@@ -1,7 +1,21 @@
 library(dplyr)
+
 library(readr)
 
-read_file('life-under-lockdown-wave2-extract.xlsx')  
+
+lul2_d <- read_file('life-under-lockdown-wave2-extract.xlsx')
+
+lul2_d %>%
+   mutate(know_facebook = ordered(
+  know_facebook,
+  levels = c(
+    'Nothing at all',
+    'A fair amount',
+    'A great deal'
+    ))) %>%
+  group_by(know_facebook) %>%
+summarise(n = n(), Beliefs = mean(Beliefs))  
+
 
 
 
